9c8dfda4d449705b13ab86978ee12184a20a3f61
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-08-17 19:56:12 +0200

    The udp4 test should not use IPv6.

--- args-client-udp4.pl
+++ args-client-udp4.pl
@@ -1,5 +1,5 @@
 # test with default values
-# The client writes a message to a localhost IPv6 UDP socket.
+# The client writes a message to a localhost IPv4 UDP socket.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via udp to the loghost.
 # The server receives the message on its udp socket.
@@ -10,7 +10,7 @@ use warnings;
 
 our %args = (
     client => {
-	connect => { domain => AF_INET6, addr => "::1", port => 514 },
+	connect => { domain => AF_INET, addr => "127.0.0.1", port => 514 },
     },
     syslogd => {
 	options => ["-u"],
