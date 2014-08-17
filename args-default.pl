011d02c2692ce76a0b57b33b6f5f9a37c051521e
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-08-17 14:51:05 +0200

    Describe what the default test really does.

--- args-default.pl
+++ args-default.pl
@@ -1,1 +1,14 @@
+# test with default values
+# The client writes a message to the log socket.
+# The syslogd writes it into a file and through a pipe.
+# The syslogd passes it via udp to the loghost.
+# The server receives the message on its udp socket.
+# Find the message in client, file, pipe, syslogd, server log.
+
+use strict;
+use warnings;
+
+our %args = (
+);
+
 1;
