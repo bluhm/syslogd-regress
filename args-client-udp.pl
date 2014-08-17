043dc9937c98811eda96a0d4396c965b0f9c5a82
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-08-17 19:54:20 +0200

    Test several methods for a client to write a log message via UDP.

---
+++ args-client-udp.pl
@@ -0,0 +1,20 @@
+# test with default values
+# The client writes a message to Sys::Syslog UDP method.
+# The syslogd writes it into a file and through a pipe.
+# The syslogd passes it via udp to the loghost.
+# The server receives the message on its udp socket.
+# Find the message in client, file, pipe, syslogd, server log.
+
+use strict;
+use warnings;
+
+our %args = (
+    client => {
+	logsock => { type => "udp", host => "127.0.0.1", port => 514 },
+    },
+    syslogd => {
+	options => ["-u"],
+    }
+);
+
+1;
---
+++ args-client-udp6.pl
@@ -0,0 +1,20 @@
+# test with default values
+# The client writes a message to a localhost IPv6 UDP socket.
+# The syslogd writes it into a file and through a pipe.
+# The syslogd passes it via udp to the loghost.
+# The server receives the message on its udp socket.
+# Find the message in client, file, pipe, syslogd, server log.
+
+use strict;
+use warnings;
+
+our %args = (
+    client => {
+	connect => { domain => AF_INET6, addr => "::1", port => 514 },
+    },
+    syslogd => {
+	options => ["-u"],
+    }
+);
+
+1;
