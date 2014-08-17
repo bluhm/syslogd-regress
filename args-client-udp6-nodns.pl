42d3a08f2ef35f1e25e0cd4389a051252f6df865
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-08-17 21:39:14 +0200

    Better test description.

--- args-client-udp6-nodns.pl
+++ args-client-udp6-nodns.pl
@@ -1,9 +1,10 @@
 # test with default values
 # The client writes a message to a localhost IPv6 UDP socket.
-# The syslogd writes it into a file and through a pipe.
+# The syslogd writes it into a file and through a pipe without dns.
 # The syslogd passes it via udp to the loghost.
 # The server receives the message on its udp socket.
 # Find the message in client, file, pipe, syslogd, server log.
+# Check that the file log contains a localhost ip address.
 
 use strict;
 use warnings;
