e4323a0bb022d2ca0502246252784cd34e6768e3
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-08-17 21:28:43 +0200

    Test nodns and grep for IPv4 or IPv6 addresses.

---
+++ args-client-udp-nodns.pl
@@ -0,0 +1,23 @@
+# test with default values
+# The client writes a message to Sys::Syslog UDP method.
+# The syslogd writes it into a file and through a pipe.
+# The syslogd passes it via udp to the loghost.
+# The server receives the message on its udp socket.
+# Find the message in client, file, pipe, syslogd, server log.
+
+use strict;
+use warnings;
+
+our %args = (
+    client => {
+	logsock => { type => "udp", host => "127.0.0.1", port => 514 },
+    },
+    syslogd => {
+	options => ["-un"],
+    },
+    file => {
+	loggrep => qr/ 127.0.0.1 syslogd-regress\[\d+\]: /. get_log(),
+    },
+);
+
+1;
---
+++ args-client-udp6-nodns.pl
@@ -0,0 +1,23 @@
+# test with default values
+# The client writes a message to a localhost IPv6 UDP socket.
+# The syslogd writes it into a file and through a pipe.
+# The syslogd passes it via udp to the loghost.
+# The server receives the message on its udp socket.
+# Find the message in client, file, pipe, syslogd, server log.
+
+use strict;
+use warnings;
+
+our %args = (
+    client => {
+	connect => { domain => AF_INET6, addr => "::1", port => 514 },
+    },
+    syslogd => {
+	options => ["-un"],
+    },
+    file => {
+	loggrep => qr/ ::1 /. get_log(),
+    },
+);
+
+1;
