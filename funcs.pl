194e62d75fea363aa12c92b4004539bee66480b6
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-08-17 16:06:28 +0200

    Grep for test massage in all log files.

--- funcs.pl
+++ funcs.pl
@@ -17,23 +17,27 @@
 use strict;
 use warnings;
 use Errno;
+use List::Util qw(first);
 use Socket;
 use Socket6;
 use Sys::Syslog qw(:standard :macros);
 use IO::Socket;
 use IO::Socket::INET6;
 
+my $testlog = "syslogd regress test log message";
+my $downlog = "syslogd regress client shutdown";
+
 ########################################################################
 # Client funcs
 ########################################################################
 
 sub write_log {
-	syslog(LOG_INFO, "log message from syslogd regress client");
+	syslog(LOG_INFO, $testlog);
 	write_shutdown();
 }
 
 sub write_shutdown {
-	syslog(LOG_NOTICE, "syslogd regress client shutdown");
+	syslog(LOG_NOTICE, $downlog);
 }
 
 ########################################################################
@@ -48,7 +52,7 @@ sub read_log {
 		    or die "read log line failed: $!";
 		chomp $line;
 		print STDERR ">>> $line\n";
-		last if $line =~ /syslogd regress client shutdown/;
+		last if $line =~ /$downlog/;
 	}
 }
 
@@ -62,6 +66,7 @@ sub check_logs {
 	return if $args{nocheck};
 
 	check_loggrep($c, $r, $s, %args);
+	check_outgrep($r, %args);
 }
 
 sub check_loggrep {
@@ -69,9 +74,9 @@ sub check_loggrep {
 
 	my %name2proc = (client => $c, syslogd => $r, server => $s);
 	foreach my $name (qw(client syslogd server)) {
-		my $p = $name2proc{$name}
-		    or next;
-		my $pattern = $args{$name}{loggrep} or next;
+		next if $args{$name}{nocheck};
+		my $p = $name2proc{$name} or next;
+		my $pattern = $args{$name}{loggrep} || $testlog;
 		$pattern = [ $pattern ] unless ref($pattern) eq 'ARRAY';
 		foreach my $pat (@$pattern) {
 			if (ref($pat) eq 'HASH') {
@@ -87,6 +92,39 @@ sub check_loggrep {
 			}
 		}
 	}
+}
+
+sub check_outgrep {
+	my ($r, %args) = @_;
+
+	foreach my $name (qw(file pipe)) {
+		next if $args{$name}{nocheck};
+		my $file = $r->{"out$name"} or next;
+		my $pattern = $args{$name}{loggrep} || $testlog;
+		$pattern = [ $pattern ] unless ref($pattern) eq 'ARRAY';
+		foreach my $pat (@$pattern) {
+			if (ref($pat) eq 'HASH') {
+				while (my($re, $num) = each %$pat) {
+					my @matches = filegrep($file, $re);
+					@matches == $num
+					    or die "$name matches @matches: ",
+					    "$re => $num";
+				}
+			} else {
+				filegrep($file, $pat)
+				    or die "$name log missing pattern: $pat";
+			}
+		}
+	}
+}
+
+sub filegrep {
+	my ($file, $pattern) = @_;
+
+	open(my $fh, '<', $file)
+	    or die "Open file $file for reading failed: $!";
+	return wantarray ?
+	    grep { /$pattern/ } <$fh> : first { /$pattern/ } <$fh>;
 }
 
 1;
