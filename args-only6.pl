1c12a2dabb33db6a9810ab76a39d7bc5a89509bb
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-08-18 15:00:01 +0200

    Test syslogd -6.

---
+++ args-only6.pl
@@ -0,0 +1,23 @@
+# The client writes a message to Sys::Syslog native method.
+# The syslogd writes it into a file and through a pipe.
+# The syslogd -6 passes it via IPv6 UDP to the loghost.
+# The server receives the message on its UDP socket.
+# Find the message in client, file, pipe, syslogd, server log.
+# Check that the syslogd has no IPv4 socket in fstat output.
+
+use strict;
+use warnings;
+
+our %args = (
+    syslogd => {
+	options => ["-6"],
+	fstat => 1,
+    },
+    fstat => {
+	loggrep => {
+	    qr/ internet / => 0,
+	},
+    },
+);
+
+1;
