dacc6320fd9ee8a88625f62984d6f84c8c26f270
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-08-18 14:36:07 +0200

    Make fstat for all processes and grep as syslog runs with privsep.

--- Proc.pm
+++ Proc.pm
@@ -181,20 +181,15 @@ sub up {
 	if ($self->{fstat}) {
 		open(my $fh, '>', $self->{fstatfile}) or die ref($self),
 		    " open $self->{fstatfile} for writing failed: $!";
-		my @cmd = ("fstat", "-p", $self->{pid});
-		defined(my $fpid = fork())
-		    or die ref($self), " fork fstat failed: $!";
-		if ($fpid == 0) {
-			open(STDOUT, '>&', $fh)
-			    or die ref($self), " dup fstat failed: $!";
-			close($fh);
-			exec @cmd;
-			die ref($self), " exec '@cmd' failed: $?";
-		}
-		waitpid($fpid, 0)
-		    or die ref($self), " wait fstat failed: $!";
-		$? == 0
-		    or die ref($self), " fstat failed: $?";
+		my @cmd = ("fstat");
+		open(my $fs, '-|', @cmd)
+		    or die ref($self), " open pipe from '@cmd' failed: $!";
+		print $fh grep { /^\w+ *syslogd *\d+/ } <$fs>;
+		close($fs) or die ref($self), $! ?
+		    " close pipe from '@cmd' failed: $!" :
+		    " command '@cmd' failed: $?";
+		close($fh)
+		    or die ref($self), " close $self->{fstatfile} failed: $!";
 	}
 	return $self;
 }
