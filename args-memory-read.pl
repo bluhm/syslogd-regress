124d7a83880f43dc5958da68365324d80b2cc0fb
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-09-04 22:41:11 +0200

    Fix memory comments.

--- args-memory-read.pl
+++ args-memory-read.pl
@@ -1,8 +1,8 @@
 # The client writes a message to Sys::Syslog native method.
 # The syslogd writes it into a file and through a pipe.
-# Syslogc reads the memory logs.
 # The syslogd passes it via UDP to the loghost.
 # The server receives the message on its UDP socket.
+# Syslogc reads the memory logs.
 # Find the message in client, file, pipe, syslogd, server, syslogc log.
 # Check that memory buffer has not been cleared.
 
