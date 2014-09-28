b571c8a7f274b445200d99a13673d36d4047eb90
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-09-28 15:37:47 +0200

    Rename FORW to FORWUDP.

--- args-server-udp4.pl
+++ args-server-udp4.pl
@@ -13,7 +13,7 @@ our %args = (
     syslogd => {
 	loghost => '@127.0.0.1:$connectport',
 	loggrep => {
-	    qr/Logging to FORW \@127.0.0.1:\d+/ => '>=4',
+	    qr/Logging to FORWUDP \@127.0.0.1:\d+/ => '>=4',
 	    get_testlog() => 1,
 	},
     },
