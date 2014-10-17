4c906a063cd9ee8c3ccdd54709e74a56b7d1b316
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-10-17 21:05:25 +0200

    Send TCP messages to an IPv6 server.

--- args-server-tcp6.pl
+++ args-server-tcp6.pl
@@ -1,9 +1,9 @@
 # The client writes a message to Sys::Syslog native method.
 # The syslogd writes it into a file and through a pipe.
-# The syslogd passes it via IPv4 TCP to an explicit loghost.
+# The syslogd passes it via IPv6 TCP to an explicit loghost.
 # The server receives the message on its TCP socket.
 # Find the message in client, file, pipe, syslogd, server log.
-# Check that syslogd and server log contain 127.0.0.1 address.
+# Check that syslogd and server log contain ::1 address.
 
 use strict;
 use warnings;
@@ -11,16 +11,16 @@ use Socket;
 
 our %args = (
     syslogd => {
-	loghost => '@tcp://127.0.0.1:$connectport',
+	loghost => '@tcp://[::1]:$connectport',
 	loggrep => {
-	    qr/Logging to FORWTCP \@tcp:\/\/127.0.0.1:\d+/ => '>=4',
+	    qr/Logging to FORWTCP \@tcp:\/\/\[::1\]:\d+/ => '>=4',
 	    get_testlog() => 1,
 	},
     },
     server => {
-	listen => { domain => AF_INET, protocol => "tcp", addr => "127.0.0.1" },
+	listen => { domain => AF_INET6, protocol => "tcp", addr => "::1" },
 	loggrep => {
-	    qr/listen sock: 127.0.0.1 \d+/ => 1,
+	    qr/listen sock: ::1 \d+/ => 1,
 	    get_testlog() => 1,
 	},
     },
