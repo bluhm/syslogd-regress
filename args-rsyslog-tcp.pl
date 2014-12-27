9ec89a9bc51824fe1a5d6f68d00b3f7a7ae5ab1b
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-12-27 14:52:13 +0100

    Add rsyslog test for TCP.

--- args-rsyslog-tcp.pl
+++ args-rsyslog-tcp.pl
@@ -1,8 +1,8 @@
 # Test with rsyslogd as receiver.
 # The client writes a message to Sys::Syslog native method.
 # The syslogd writes it into a file and through a pipe.
-# The syslogd passes it via UDP to the rsyslogd.
-# The rsyslogd receives the message on its UDP socket.
+# The syslogd passes it via TCP to the rsyslogd.
+# The rsyslogd receives the message on its TCP socket.
 # Find the message in client, file, pipe, syslogd, rsyslogd log.
 # Check that the message is in the rsyslogd out file.
 
@@ -11,6 +11,7 @@ use warnings;
 
 our %args = (
     rsyslogd => {
+	listen => { proto => "tcp" },
     },
 );
 
