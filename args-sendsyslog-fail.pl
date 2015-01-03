c16e0141d15a3d066272aeb1b2b781391590810d
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-01-03 16:01:40 +0100

    Check that the kernel writes an sendsyslog(2) error message.

--- args-sendsyslog-fail.pl
+++ args-sendsyslog-fail.pl
@@ -1,23 +1,36 @@
+# Run client before starting syslogd.
 # The client writes a message to Sys::Syslog native method.
-# The syslogd writes it into a file and through a pipe.
-# The syslogd passes it via UDP to the loghost.
-# The server receives the message on its UDP socket.
-# Find the message in client, file, pipe, syslogd, server log.
-# Create a ktrace dump of the client and check that sendsyslog(2)
-# has been used.
+# The kernel write an sendsyslog(2) error message to its dmesg log buffer.
+# Start syslogd, it reads the message from klog device.
+# Find the kernel error message in file, pipe, syslogd, server log.
+# Create a ktrace dump of the client and check that sendsyslog(2) has failed.
 
 use strict;
 use warnings;
 
+my $kernlog = "/bsd: send message to syslog failed";
+
 our %args = (
     client => {
+	early => 1,
 	ktrace => 1,
 	kdump => {
 	    qr/CALL  sendsyslog/ => 2,
-	    qr/GIO   fd -1 wrote \d+ bytes/ => 2,
-	    qr/RET   sendsyslog 0/ => 2,
+	    qr/RET   sendsyslog -1 errno 57 Socket is not connected/ => 2,
 	},
     },
+    syslogd => {
+	loggrep => $kernlog,
+    },
+    server => {
+	func => sub {
+	    my $self = shift;
+	    read_message($self, $kernlog, @_);
+	},
+	loggrep => $kernlog,
+    },
+    file => { loggrep => $kernlog },
+    pipe => { loggrep => $kernlog },
 );
 
 1;
