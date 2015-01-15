1a9fef380fa5e7d250a9b5cdd6019df192f465ef
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-01-15 23:37:18 +0100

    Put TLS error messages into test regex.

--- args-server-tls-close.pl
+++ args-server-tls-close.pl
@@ -1,8 +1,8 @@
-# The TCP server closes the connection to syslogd.
+# The TLS server closes the connection to syslogd.
 # The client writes a message to Sys::Syslog native method.
 # The syslogd writes it into a file and through a pipe.
-# The syslogd passes it via IPv4 TCP to an explicit loghost.
-# The server receives the message on its TCP socket.
+# The syslogd passes it via IPv4 TLS to an explicit loghost.
+# The server receives the message on its TLS socket.
 # Find the message in client, pipe, syslogd log.
 # Check that syslogd writes a log message about the server close.
 
@@ -14,33 +14,33 @@ our %args = (
     client => {
 	func => sub {
 	    my $self = shift;
-	    ${$self->{syslogd}}->loggrep("loghost .* connection close", 5)
-		or die "no connection close in syslogd.log";
+	    ${$self->{syslogd}}->loggrep("loghost .* connection error", 5)
+		or die "no connection error in syslogd.log";
 	    write_log($self, @_);
 	},
     },
     syslogd => {
-	loghost => '@tcp://127.0.0.1:$connectport',
+	loghost => '@tls://127.0.0.1:$connectport',
 	loggrep => {
-	    qr/Logging to FORWTCP \@tcp:\/\/127.0.0.1:\d+/ => '>=4',
+	    qr/Logging to FORWTLS \@tls:\/\/127.0.0.1:\d+/ => '>=4',
 	    get_testlog() => 1,
-	    qr/syslogd: loghost .* connection close/ => 2,
+	    qr/syslogd: loghost .* connection error: read failed \(5\)/ => 2,
 	},
     },
     server => {
-	listen => { domain => AF_INET, proto => "tcp", addr => "127.0.0.1" },
+	listen => { domain => AF_INET, proto => "tls", addr => "127.0.0.1" },
 	func => sub {
 	    my $self = shift;
 	    shutdown(\*STDOUT, 1)
 		or die "shutdown write failed: $!";
-	    ${$self->{syslogd}}->loggrep("loghost .* connection close", 5)
-		or die "no connection close in syslogd.log";
+	    ${$self->{syslogd}}->loggrep("loghost .* connection error", 5)
+		or die "no connection error in syslogd.log";
 	},
 	loggrep => {},
     },
     file => {
 	loggrep => {
-	    qr/syslogd: loghost .* connection close/ => 1,
+	    qr/syslogd: loghost .* connection error: read failed \(5\)/ => 1,
 	},
     },
 );
