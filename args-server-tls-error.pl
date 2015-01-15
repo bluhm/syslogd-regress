1833327b108d93457c6283bedc537c13b26717c7
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-01-16 00:07:32 +0100

    Replace TCP with TLS.

--- args-server-tls-error.pl
+++ args-server-tls-error.pl
@@ -1,18 +1,14 @@
-# The TCP server aborts the connection to syslogd.
+# The TLS server aborts the connection to syslogd.
 # The client writes a message to Sys::Syslog native method.
 # The syslogd writes it into a file and through a pipe.
-# The syslogd passes it via IPv4 TCP to an explicit loghost.
-# The server receives the message on its TCP socket.
+# The syslogd passes it via IPv4 TLS to an explicit loghost.
+# The server receives the message on its TLS socket.
 # Find the message in client, pipe, syslogd log.
 # Check that syslogd writes a log message about the server error.
 
 use strict;
 use warnings;
 use Socket;
-use Errno ':POSIX';
-
-my @errors = (EPIPE,ECONNRESET);
-my $errors = "(". join("|", map { $! = $_ } @errors). ")";
 
 our %args = (
     client => {
@@ -24,15 +20,15 @@ our %args = (
 	},
     },
     syslogd => {
-	loghost => '@tcp://127.0.0.1:$connectport',
+	loghost => '@tls://127.0.0.1:$connectport',
 	loggrep => {
-	    qr/Logging to FORWTCP \@tcp:\/\/127.0.0.1:\d+/ => '>=4',
+	    qr/Logging to FORWTLS \@tls:\/\/127.0.0.1:\d+/ => '>=4',
 	    get_testlog() => 1,
 	    qr/syslogd: loghost .* connection error/ => 2,
 	},
     },
     server => {
-	listen => { domain => AF_INET, proto => "tcp", addr => "127.0.0.1" },
+	listen => { domain => AF_INET, proto => "tls", addr => "127.0.0.1" },
 	func => sub {
 	    my $self = shift;
 	    setsockopt(STDOUT, SOL_SOCKET, SO_LINGER, pack('ii', 1, 0))
@@ -42,7 +38,7 @@ our %args = (
     },
     file => {
 	loggrep => {
-	    qr/syslogd: loghost .* connection error: $errors/
+	    qr/syslogd: loghost .* connection error: read failed \(5\)/
 		=> 1,
 	},
     },
