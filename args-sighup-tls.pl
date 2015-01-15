35fd3d918129b0239e5a1c343e15d556dae6e01e
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-01-16 00:14:57 +0100

    Replace TCP with TLS.

--- args-sighup-tls.pl
+++ args-sighup-tls.pl
@@ -1,9 +1,9 @@
 # The client writes a message to Sys::Syslog native method.
 # The syslogd writes it into a file and through a pipe.
-# The syslogd passes it via TCP to the loghost.
-# The server receives the message on its TCP socket.
+# The syslogd passes it via TLS to the loghost.
+# The server receives the message on its TLS socket.
 # Find the message in client, file, pipe, syslogd, server log.
-# Check that a SIGHUP reconnects the TCP stream and closes the socket.
+# Check that a SIGHUP reconnects the TLS stream and closes the socket.
 
 use strict;
 use warnings;
@@ -26,7 +26,7 @@ our %args = (
 	    qr/syslogd  PSIG  SIGHUP caught handler/ => 1,
 	    qr/syslogd  RET   execve 0/ => 1,
 	},
-	loghost => '@tcp://127.0.0.1:$connectport',
+	loghost => '@tls://127.0.0.1:$connectport',
 	loggrep => {
 	    qr/config file changed: dying/ => 0,
 	    qr/config file modified: restarting/ => 0,
@@ -35,7 +35,7 @@ our %args = (
 	},
     },
     server => {
-	listen => { domain => AF_INET, addr => "127.0.0.1", proto => "tcp" },
+	listen => { domain => AF_INET, addr => "127.0.0.1", proto => "tls" },
 	redo => 0,
 	func => sub {
 	    my $self = shift;
