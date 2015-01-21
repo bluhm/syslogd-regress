2e351d291426f9c9454914190edb745d069e8c34
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-01-22 00:18:28 +0100

    The cafile is too big.

--- args-tls-cafile-big.pl
+++ args-tls-cafile-big.pl
@@ -1,7 +1,7 @@
 # The client writes a message to Sys::Syslog native method.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via TLS to localhost loghost.
-# The cafile does not exist.
+# The cafile is too big.
 # Find the message in client, file, pipe, syslogd log.
 # Check that syslogd has verify failure and server has no message.
 
@@ -13,12 +13,12 @@ our %args = (
     syslogd => {
 	loghost => '@tls://localhost:$connectport',
 	loggrep => {
-	    qr/open CAfile: No such file or directory/ => 2,
+	    qr/CAfile larger than 1GB/ => 2,
 	    qr/Logging to FORWTLS \@tls:\/\/localhost:\d+/ => '>=4',
 	    qr/error: ssl verify setup failure/ => 2,
 	    get_testlog() => 1,
 	},
-	cacrt => "noexist",
+	cacrt => "big",
     },
     server => {
 	listen => { domain => AF_UNSPEC, proto => "tls", addr => "localhost" },
