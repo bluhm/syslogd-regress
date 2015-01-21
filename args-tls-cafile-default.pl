cfde39bdf437d4f69969f21ce86fd909a81b9e0d
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-01-22 00:30:27 +0100

    The cafile is the system default which has no matching cert.

--- args-tls-cafile-default.pl
+++ args-tls-cafile-default.pl
@@ -1,7 +1,7 @@
 # The client writes a message to Sys::Syslog native method.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via TLS to localhost loghost.
-# The cafile is empty.
+# The cafile is the system default which has no matching cert.
 # Find the message in client, file, pipe, syslogd log.
 # Check that syslogd has verify failure and server has no message.
 
@@ -13,12 +13,12 @@ our %args = (
     syslogd => {
 	loghost => '@tls://localhost:$connectport',
 	loggrep => {
-	    qr/CAfile empty, size 0/ => 1,
+	    qr/CAfile \/etc\/ssl\/cert.pem/ => 1,
 	    qr/Logging to FORWTLS \@tls:\/\/localhost:\d+/ => '>=4',
-	    qr/error: ssl verify memory setup failure/ => 2,
+	    qr/connection error: SSL connect failed: 1/ => 2,
 	    get_testlog() => 1,
 	},
-	cacrt => "empty",
+	cacrt => "default",
     },
     server => {
 	listen => { domain => AF_UNSPEC, proto => "tls", addr => "localhost" },
