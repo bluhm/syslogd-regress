9244f9c2ccd6d889e5f5bcf73b9438e27fde8246
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-01-21 23:33:25 +0100

    Check that syslogd denies host `127.0.0.1'.

--- args-tls-bad-host.pl
+++ args-tls-bad-host.pl
@@ -1,9 +1,9 @@
 # The client writes a message to Sys::Syslog native method.
 # The syslogd writes it into a file and through a pipe.
-# The syslogd passes it via IPv4 TLS to an explicit loghost.
-# The server receives the message on its TLS socket.
-# Find the message in client, file, pipe, syslogd, server log.
-# Check that syslogd and server log contain 127.0.0.1 address.
+# The syslogd passes it via TLS to 127.0.0.1 loghost.
+# Server certificate is issued for localhost.
+# Find the message in client, file, pipe, syslogd log.
+# Check that syslogd denies host `127.0.0.1' and server has no message.
 
 use strict;
 use warnings;
@@ -14,14 +14,16 @@ our %args = (
 	loghost => '@tls://127.0.0.1:$connectport',
 	loggrep => {
 	    qr/Logging to FORWTLS \@tls:\/\/127.0.0.1:\d+/ => '>=4',
+	    qr/error: host `127.0.0.1' not present in server/ => '>=1',
 	    get_testlog() => 1,
 	},
+	cacrt => "ca.crt",
     },
     server => {
 	listen => { domain => AF_INET, proto => "tls", addr => "127.0.0.1" },
 	loggrep => {
 	    qr/listen sock: 127.0.0.1 \d+/ => 1,
-	    get_testlog() => 1,
+	    get_testlog() => 0,
 	},
     },
 );
