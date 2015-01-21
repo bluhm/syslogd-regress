1e38ed0aa4b0eb39944ff5d0c90482d9f0054a43
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-01-22 00:39:03 +0100

    The cafile is a fake ca with correct DN but wrong key.

--- args-tls-cafile-fake.pl
+++ args-tls-cafile-fake.pl
@@ -1,7 +1,7 @@
 # The client writes a message to Sys::Syslog native method.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via TLS to localhost loghost.
-# The cafile is the system default which has no matching cert.
+# The cafile is a fake ca with correct DN but wrong key.
 # Find the message in client, file, pipe, syslogd log.
 # Check that syslogd has verify failure and server has no message.
 
@@ -13,12 +13,12 @@ our %args = (
     syslogd => {
 	loghost => '@tls://localhost:$connectport',
 	loggrep => {
-	    qr/CAfile \/etc\/ssl\/cert.pem/ => 1,
+	    qr/CAfile fake-ca.crt/ => 1,
 	    qr/Logging to FORWTLS \@tls:\/\/localhost:\d+/ => '>=4',
 	    qr/connection error: SSL connect failed: 1/ => 2,
 	    get_testlog() => 1,
 	},
-	cacrt => "default",
+	cacrt => "fake-ca.crt",
     },
     server => {
 	listen => { domain => AF_UNSPEC, proto => "tls", addr => "localhost" },
