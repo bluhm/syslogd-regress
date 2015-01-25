5de16417f70aee95e92bade27ee91dfa02d4cccd
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-01-26 00:52:05 +0100

    Count 'syslogd: started', it must be twice in logfile.

--- Proc.pm
+++ Proc.pm
@@ -148,7 +148,7 @@ sub wait {
 
 sub loggrep {
 	my $self = shift;
-	my($regex, $timeout) = @_;
+	my($regex, $timeout, $count) = @_;
 	my $exit = ($self->{exit} // 0) << 8;
 
 	my $end = time() + $timeout if $timeout;
@@ -162,7 +162,8 @@ sub loggrep {
 		open(my $fh, '<', $self->{logfile})
 		    or die ref($self), " log file open failed: $!";
 		my @match = grep { /$regex/ } <$fh>;
-		return wantarray ? @match : $match[0] if @match;
+		return wantarray ? @match : $match[0]
+		    if !$count && @match or $count && @match >= $count;
 		close($fh);
 		# pattern not found
 		if ($kid == 0) {
