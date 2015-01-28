06fcaff53f67bccc5d65c317cb402885663d858a
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-01-28 21:12:55 +0100

    Check that server log contains the no shared cipher error.

--- args-tls-cipher-null.pl
+++ args-tls-cipher-null.pl
@@ -1,9 +1,9 @@
 # The client writes a message to Sys::Syslog native method.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via TLS to localhost loghost.
-# The server receives the message on its TLS socket.
-# Find the message in client, file, pipe, syslogd, server log.
-# Check that syslogd and server log contain localhost address.
+# The server offers only the null cipher on its TLS socket.
+# Find the message in client, file, pipe, syslogd log.
+# Check that server log contains the no shared cipher error.
 
 use strict;
 use warnings;
@@ -20,9 +20,14 @@ our %args = (
     },
     server => {
 	listen => { domain => AF_UNSPEC, proto => "tls", addr => "localhost" },
+	sslciphers => "NULL",
+	up => "IO::Socket::SSL socket accept failed",
+	down => "Server",
+	exit => 255,
 	loggrep => {
 	    qr/listen sock: (127.0.0.1|::1) \d+/ => 1,
-	    get_testlog() => 1,
+	    qr/no shared cipher/ => 1,
+	    get_testlog() => 0,
 	},
     },
 );
