0df422fbc52fbc7ae565f20cab6f422d33eb9632
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-02-01 21:52:40 +0100

    Send long lines via TCP.

--- args-length-tcp.pl
+++ args-length-tcp.pl
@@ -1,7 +1,7 @@
 # The client writes long messages to UDP socket.
 # The syslogd writes it into a file and through a pipe.
-# The syslogd passes it via UDP to the loghost.
-# The server receives the message on its UDP socket.
+# The syslogd passes it via TCP to the loghost.
+# The server receives the message on its TCP socket.
 # Find the message in client, file, pipe, syslogd, server log.
 # Check that lines in file have 8192 bytes message length after the header.
 # Check that lines in server have 8192 bytes line length.
@@ -19,15 +19,19 @@ our %args = (
 	lengths => [ 8190..8193,9000 ],
     },
     syslogd => {
+	loghost => '@tcp://localhost:$connectport',
 	options => ["-u"],
 	loggrep => {
 	    $msg => 5,
 	}
     },
     server => {
-	# >>> <13>Jan 31 00:10:11 0123456789ABC...lmn
+	listen => { domain => AF_UNSPEC, proto => "tcp", addr => "localhost" },
+	# >>> <13>Jan 31 00:10:11 0123456789ABC...567
 	loggrep => {
-	    qr/^>>> .{8192}$/ => 5,
+	    qr/^>>> .{19} .{8190}$/ => 1,
+	    qr/^>>> .{19} .{8191}$/ => 1,
+	    qr/^>>> .{19} .{8192}$/ => 3,
 	},
     },
     file => {
