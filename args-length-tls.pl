21bb288bb7fd7699c6d0abec348a5edcee83860a
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-02-01 22:08:31 +0100

    Replace TCP with TLS.

--- args-length-tls.pl
+++ args-length-tls.pl
@@ -1,7 +1,7 @@
 # The client writes long messages to UDP socket.
 # The syslogd writes it into a file and through a pipe.
-# The syslogd passes it via TCP to the loghost.
-# The server receives the message on its TCP socket.
+# The syslogd passes it via TLS to the loghost.
+# The server receives the message on its TLS socket.
 # Find the message in client, file, pipe, syslogd, server log.
 # Check that lines in server have 8192 bytes message length.
 
@@ -18,14 +18,14 @@ our %args = (
 	lengths => [ 8190..8193,9000 ],
     },
     syslogd => {
-	loghost => '@tcp://localhost:$connectport',
+	loghost => '@tls://localhost:$connectport',
 	options => ["-u"],
 	loggrep => {
 	    $msg => 5,
 	}
     },
     server => {
-	listen => { domain => AF_UNSPEC, proto => "tcp", addr => "localhost" },
+	listen => { domain => AF_UNSPEC, proto => "tls", addr => "localhost" },
 	# >>> <13>Jan 31 00:10:11 0123456789ABC...567
 	loggrep => {
 	    $msg => 5,
