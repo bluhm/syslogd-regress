7f15111a8e7c7b6c7b5d022af2f260e24748e4a4
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-02-11 21:40:57 +0100

    Allow a range in the dropped messages log.

--- args-dropped-sighup-tcp.pl
+++ args-dropped-sighup-tcp.pl
@@ -57,7 +57,7 @@ our %args = (
 	    qr/syslogd: start/ => 1,
 	    qr/syslogd: restart/ => 1,
 	    get_charlog() => 43,
-	    qr/syslogd: dropped 259 messages to remote loghost/ => 1,
+	    qr/syslogd: dropped 2[56][0-9] messages to remote loghost/ => 1,
 	},
     },
     file => {
@@ -68,7 +68,7 @@ our %args = (
 	    qr/syslogd: start/ => 1,
 	    qr/syslogd: restart/ => 1,
 	    get_charlog() => 300,
-	    qr/syslogd: dropped 259 messages to remote loghost/ => 1,
+	    qr/syslogd: dropped 2[56][0-9] messages to remote loghost/ => 1,
 	},
     },
 );
