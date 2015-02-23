76e30546dc9830ff8f8a6568de638c5dd56895d7
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-02-23 01:05:35 +0100

    Allow a wider range in the dropped number messages log.

--- args-dropped-sigterm-tcp.pl
+++ args-dropped-sigterm-tcp.pl
@@ -63,7 +63,7 @@ our %args = (
 	    get_testlog() => 0,
 	    qr/syslogd: start/ => 1,
 	    get_charlog() => 300,
-	    qr/syslogd: dropped 2[56][0-9] messages to remote loghost/ => 1,
+	    qr/syslogd: dropped 2[0-9][0-9] messages to remote loghost/ => 1,
 	},
     },
 );
