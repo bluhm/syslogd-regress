9ebe0712d2ad035d1d12543dd67c860cb8ed4447
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-06-12 02:35:35 +0200

    Check that fstat contains a bound UDP socket.

--- args-client-bind.pl
+++ args-client-bind.pl
@@ -1,9 +1,11 @@
+# Syslog binds UDP socket to 127.0.0.1.
 # The client writes a message to Sys::Syslog UDP method.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via UDP to the loghost.
 # The server receives the message on its UDP socket.
 # Find the message in client, file, pipe, syslogd, server log.
 # Check that the file log contains the localhost name.
+# Check that fstat contains a bound UDP socket.
 
 use strict;
 use warnings;
@@ -13,10 +15,14 @@ our %args = (
 	logsock => { type => "udp", host => "127.0.0.1", port => 514 },
     },
     syslogd => {
-	options => ["-u"],
+	options => ["-U", "127.0.0.1"],
+	fstat => 1,
     },
     file => {
 	loggrep => qr/ localhost syslogd-regress\[\d+\]: /. get_testlog(),
+    },
+    fstat => {
+	loggrep => qr/ internet dgram udp 127.0.0.1:514$/,
     },
 );
 
