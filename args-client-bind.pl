78df09bcb228edd8774f85346616c1f82284dae5
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-06-25 23:50:31 +0200

    Bind to localhost, 127.0.0.1, ::1 with and without port.

--- args-client-bind.pl
+++ args-client-bind.pl
@@ -1,5 +1,5 @@
 # Syslog binds UDP socket to localhost.
-# The client writes a message to Sys::Syslog UDP method.
+# The client writes a message to a localhost UDP socket.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via UDP to the loghost.
 # The server receives the message on its UDP socket.
@@ -21,6 +21,9 @@ our %args = (
     syslogd => {
 	options => ["-U", "localhost"],
 	fstat => qr/ internet6? dgram udp (127.0.0.1|\[::1\]):514$/,
+    },
+    file => {
+	loggrep => qr/ localhost /. get_testlog(),
     },
 );
 
