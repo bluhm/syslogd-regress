3d6e266afde0443c01f5a53b88d14106b621c1d1
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-06-27 22:16:02 +0200

    Adapt comment.

--- args-block-prog.pl
+++ args-block-prog.pl
@@ -1,9 +1,10 @@
-# Test with default values, that is:
 # The client writes a message to Sys::Syslog native method.
+# The syslogd writes into multiple files depending on program name.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via UDP to the loghost.
 # The server receives the message on its UDP socket.
 # Find the message in client, file, pipe, syslogd, server log.
+# Check that the message appears in the correct log files.
 
 use strict;
 use warnings;
