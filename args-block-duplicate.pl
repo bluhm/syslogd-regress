a195c78860f534117f76d7fd71988bef03df47ff
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-06-28 00:08:58 +0200

    Test all combinations of files in different blocks.

--- args-block-duplicate.pl
+++ args-block-duplicate.pl
@@ -1,10 +1,10 @@
 # The client writes a message to Sys::Syslog native method.
-# The syslogd writes into multiple files depending on program and hostname.
+# The syslogd writes into duplicate files depending on program and hostname.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via UDP to the loghost.
 # The server receives the message on its UDP socket.
 # Find the message in client, file, pipe, syslogd, server log.
-# Check that the message appears in the correct log files.
+# Check that the message exaclty appears in the files allowed by both blocks.
 
 use strict;
 use warnings;
@@ -20,27 +20,29 @@ our %args = (
 !nonexist
 +nonexist
 *.*	$objdir/file-0.log
+*.*	$objdir/file-1.log
+*.*	$objdir/file-2.log
+*.*	$objdir/file-3.log
 !syslogd-regress
 *.*	$objdir/file-1.log
+*.*	$objdir/file-2.log
+*.*	$objdir/file-4.log
+*.*	$objdir/file-5.log
 +$hostname
 *.*	$objdir/file-2.log
-!nonexist
 *.*	$objdir/file-3.log
-+nonexist
-*.*	$objdir/file-4.log
-!*
 *.*	$objdir/file-5.log
-+*
 *.*	$objdir/file-6.log
 EOF
     },
+    # exactly the files in the last block get the log message
     multifile => [
 	{ loggrep => { get_testlog() => 0 } },
 	{ loggrep => { get_testlog() => 0 } },
 	{ loggrep => { get_testlog() => 1 } },
+	{ loggrep => { get_testlog() => 1 } },
 	{ loggrep => { get_testlog() => 0 } },
-	{ loggrep => { get_testlog() => 0 } },
-	{ loggrep => { get_testlog() => 0 } },
+	{ loggrep => { get_testlog() => 1 } },
 	{ loggrep => { get_testlog() => 1 } },
     ],
 );
