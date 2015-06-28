1c5ef806355bab373934e5c2d7ae7e0ee3b7e2bf
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-06-28 23:00:03 +0200

    Create sevetal messages with different facility and priority.

--- args-severity.pl
+++ args-severity.pl
@@ -1,26 +1,38 @@
-# The client writes a message to Sys::Syslog native method.
-# The syslogd writes into multiple files depending on program name.
+# The client sends messages with different facility and priority.
+# The syslogd writes into multiple files depending on severity.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via UDP to the loghost.
 # The server receives the message on its UDP socket.
 # Find the message in client, file, pipe, syslogd, server log.
-# Check that the message appears in the correct log files.
+# Check that the messages appears in the correct log files.
 
 use strict;
 use warnings;
 use Cwd;
+use Sys::Syslog;
 
 my $objdir = getcwd();
 
 our %args = (
+    client => {
+	func => sub {
+	    my $self = shift;
+	    foreach my $fac (qw(local5 local6 local7)) {
+		foreach my $sev (qw(notice warning err)) {
+		    no strict 'refs';
+		    my $facility = ("Sys::Syslog::LOG_".uc($fac))->();
+		    my $severity = ("Sys::Syslog::LOG_".uc($sev))->();
+		    syslog($facility|$severity, "$fac.$sev");
+		}
+	    }
+	    write_log($self);
+	},
+    },
     syslogd => {
 	conf => <<"EOF",
-!nonexist
 *.*	$objdir/file-0.log
-!syslogd-regress
 *.*	$objdir/file-1.log
 *.*	$objdir/file-2.log
-!*
 *.*	$objdir/file-3.log
 EOF
     },
