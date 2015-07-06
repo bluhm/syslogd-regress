f6db795c9b5c78600dd8cff538bf2deb6051f38f
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-07-06 22:28:38 +0200

    Write long line over TCP an check results.

--- args-client-tcp-maxline.pl
+++ args-client-tcp-maxline.pl
@@ -1,19 +1,14 @@
 # The syslogd listens on 127.0.0.1 TCP socket.
-# The client writes three lines into a 127.0.0.1 TCP socket in a single chunk.
+# The client writes long line into a 127.0.0.1 TCP socket.
 # The syslogd writes it into a file and through a pipe.
-# The syslogd passes it via UDP to the loghost.
-# The server receives the message on its UDP socket.
-# Find the message in file, pipe, syslogd, server log.
-# Check that the file log contains all messages.
+# The syslogd passes it via TCP to the loghost.
+# The server receives the message on its TCP socket.
+# Find the message in file, syslogd, server log.
+# Check that the file log contains the truncated message.
 
 use strict;
 use warnings;
-
-my %threegrep = (
-    get_firstlog() => 1,
-    get_secondlog() => 1,
-    get_thirdlog() => 1,
-);
+use constant MAXLINE => 8192;
 
 our %args = (
     client => {
@@ -21,19 +16,29 @@ our %args = (
 	    port => 514 },
 	func => sub {
             my $self = shift;
-	    my $msg = get_firstlog()."\n".get_secondlog()."\n".get_thirdlog();
-	    write_message($self, $msg);
+	    local $| = 1;
+	    my $msg = generate_chars($self, MAXLINE+1);
+	    print $msg;
+	    print STDERR "<<< $msg\n";
+	    ${$self->{syslogd}}->loggrep("tcp logger .* incomplete line", 5)
+		or die ref($self), " syslogd did not receive incomplete line";
 	    write_shutdown($self);
 	},
 	loggrep => {},
     },
     syslogd => {
 	options => ["-T", "127.0.0.1:514"],
-	loggrep => \%threegrep,
+	loghost => '@tcp://127.0.0.1:$connectport',
+	loggrep => qr/incomplete line, use /.(MAXLINE+1).qr/ bytes/,
     },
-    server => { loggrep => \%threegrep },
-    file => { loggrep => \%threegrep },
-    pipe => { loggrep => \%threegrep },
+    server => {
+	listen => { domain => AF_INET, proto => "tcp", addr => "127.0.0.1" },
+	loggrep => generate_chars(undef, MAXLINE).qr/$/,
+    },
+    file => {
+	loggrep => generate_chars(undef, MAXLINE).qr/$/,
+    },
+    pipe => { loggrep => {} },  # XXX syslogd ignore short writes to pipe
 );
 
 1;
