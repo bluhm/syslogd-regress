d3e1b36915cf3ac6877e88f9cb63ecb7a2a5d0f7
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-07-06 14:30:07 +0200

    Create MAXTCP connections and test maximum reached and reopen.

--- args-maxtcp.pl
+++ args-maxtcp.pl
@@ -1,4 +1,4 @@
-# The syslogd listens on 127.0.0.1 TCP socket.
+# The syslogd listens on localhost TCP socket.
 # The client writes a message into a 127.0.0.1 TCP socket.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via UDP to the loghost.
@@ -8,12 +8,47 @@
 
 use strict;
 use warnings;
+use IO::Socket::INET6;
 use constant MAXTCP => 20;
 
 our %args = (
     client => {
 	connect => { domain => AF_UNSPEC, proto => "tcp", addr => "localhost",
 	    port => 514 },
+	func => sub {
+	    my $self = shift;
+	    my @s;
+	    $s[0] = \*STDOUT;
+	    # open all additional connections and one more
+	    for (my $i = 1; $i <= MAXTCP; $i++) {
+		$s[$i] = IO::Socket::INET6->new(
+		    Domain              => AF_UNSPEC,
+		    Proto               => "tcp",
+		    PeerAddr            => "localhost",
+		    PeerPort            => 514,
+		) or die "tcp socket $i connect failed: $!";
+	    }
+	    # the last connection was denied
+	    defined $s[MAXTCP]->getline()
+		and die "MAXTCP connection not closed by syslogd\n";
+	    # close and reopen a single connection
+	    close($s[1])
+		or die "tcp socket 1 close failed: $!";
+	    $s[1] = IO::Socket::INET6->new(
+		Domain              => AF_UNSPEC,
+		Proto               => "tcp",
+		PeerAddr            => "localhost",
+		PeerPort            => 514,
+	    ) or die "tcp socket 1 connect again failed: $!";
+	    # write messages over all connections
+	    for (my $i = 0; $i < MAXTCP; $i++) {
+		my $msg = get_testlog(). " $i tcp socket";
+		my $fh = $s[$i];
+		print $fh "$msg\n";
+		print STDERR "<<< $msg\n";
+	    }
+	    write_shutdown($self);
+	},
     },
     syslogd => {
 	options => ["-T", "localhost:514"],
@@ -22,9 +57,15 @@ our %args = (
 	    qr/^_syslogd .* internet/ => 3,
 	    qr/ internet6? stream tcp \w+ (127.0.0.1|\[::1\]):514$/ => 1,
 	},
+	loggrep => {
+	    qr/tcp logger .* accepted/ => MAXTCP+1,
+	    qr/tcp logger .* denied: maximum /.MAXTCP.qr/ reached/ => 1,
+	},
     },
     file => {
-	loggrep => qr/ localhost /. get_testlog(),
+	loggrep => {
+	    qr/ localhost /.get_testlog().qr/ \d+ tcp socket/ => MAXTCP,
+	},
     },
 );
 
