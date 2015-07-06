24e41714f5dbe98597924d302216a8e1c5096d5a
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-07-06 20:29:09 +0200

    Log a TCP message that comes in multiple chunks.

--- args-client-tcp-chunk.pl
+++ args-client-tcp-chunk.pl
@@ -1,10 +1,10 @@
 # The syslogd listens on 127.0.0.1 TCP socket.
-# The client writes a message into a 127.0.0.1 TCP socket.
+# The client writes a message into a 127.0.0.1 TCP socket in multiple chunks.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via UDP to the loghost.
 # The server receives the message on its UDP socket.
 # Find the message in client, file, pipe, syslogd, server log.
-# Check that the file log contains the hostname and message.
+# Check that the file log contains the complete message.
 
 use strict;
 use warnings;
@@ -13,21 +13,24 @@ our %args = (
     client => {
 	connect => { domain => AF_INET, proto => "tcp", addr => "127.0.0.1",
 	    port => 514 },
-	loggrep => {
-	    qr/connect sock: 127.0.0.1 \d+/ => 1,
-	    get_testlog() => 1,
+	func => sub {
+            my $self = shift;
+	    local $| = 1;
+	    my $n = 0;
+	    foreach (get_testlog() =~ /.{1,5}/g) {
+		$n += length;
+		print;
+		print STDERR "<<< $_\n";
+		${$self->{syslogd}}->loggrep("tcp logger .* buffer $n bytes", 5)
+		    or die ref($self), " syslogd did not receive $n bytes";
+	    }
+	    print "\n";
+	    write_shutdown($self);
 	},
+	loggrep => {},
     },
     syslogd => {
 	options => ["-T", "127.0.0.1:514"],
-	fstat => {
-	    qr/^root .* internet/ => 0,
-	    qr/^_syslogd .* internet/ => 3,
-	    qr/ internet stream tcp \w+ 127.0.0.1:514$/ => 1,
-	},
-    },
-    file => {
-	loggrep => qr/ localhost /. get_testlog(),
     },
 );
 
