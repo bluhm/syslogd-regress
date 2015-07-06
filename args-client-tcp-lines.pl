f6b5adc0c53643a45399ada39a80f80adb007fe8
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-07-06 20:33:00 +0200

    Copy args-client-tcp-chunks.pl to args-client-tcp-lines.pl.

---
+++ args-client-tcp-lines.pl
@@ -0,0 +1,37 @@
+# The syslogd listens on 127.0.0.1 TCP socket.
+# The client writes a message into a 127.0.0.1 TCP socket in multiple chunks.
+# The syslogd writes it into a file and through a pipe.
+# The syslogd passes it via UDP to the loghost.
+# The server receives the message on its UDP socket.
+# Find the message in client, file, pipe, syslogd, server log.
+# Check that the file log contains the complete message.
+
+use strict;
+use warnings;
+
+our %args = (
+    client => {
+	connect => { domain => AF_INET, proto => "tcp", addr => "127.0.0.1",
+	    port => 514 },
+	func => sub {
+            my $self = shift;
+	    local $| = 1;
+	    my $n = 0;
+	    foreach (get_testlog() =~ /.{1,5}/g) {
+		$n += length;
+		print;
+		print STDERR "<<< $_\n";
+		${$self->{syslogd}}->loggrep("tcp logger .* buffer $n bytes", 5)
+		    or die ref($self), " syslogd did not receive $n bytes";
+	    }
+	    print "\n";
+	    write_shutdown($self);
+	},
+	loggrep => {},
+    },
+    syslogd => {
+	options => ["-T", "127.0.0.1:514"],
+    },
+);
+
+1;
