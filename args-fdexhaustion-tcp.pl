db108200142b58bc7bbda89445b8cc74cd9790f3
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-07-20 03:17:17 +0200

    Do not accept TCP connection because file descriptors are exhausted.

--- args-fdexhaustion-tcp.pl
+++ args-fdexhaustion-tcp.pl
@@ -1,11 +1,7 @@
 # The syslogd is started with reduced file descriptor limits.
 # The syslogd config contains more log files than possible.
-# The client writes a message to Sys::Syslog native method.
-# The syslogd writes it into a file and through a pipe.
-# The syslogd passes it via UDP to the loghost.
-# The server receives the message on its UDP socket.
-# Find the message in client, file, pipe, syslogd, server log.
-# Check the error messages and multiple log file content.
+# The client connects to the 127.0.0.1 TCP socket, but is not accepted.
+# Check the error messages and that syslogd tries to listen again.
 
 use strict;
 use warnings;
@@ -14,22 +10,42 @@ use Cwd;
 my $objdir = getcwd();
 
 our %args = (
+    client => {
+	connect => { domain => AF_INET, proto => "tcp", addr => "127.0.0.1",
+	    port => 514 },
+	logsock => { type => "tcp", host => "127.0.0.1", port => 514 },
+	func => sub {
+	    my $self = shift;
+	    ${$self->{syslogd}}->loggrep("Listen again", 5);
+	    write_log($self);
+	},
+    },
     syslogd => {
+	options => ["-T", "127.0.0.1:514"],
 	conf => join("", map { "*.*\t$objdir/file-$_.log\n" } 0..19),
 	rlimit => {
 	    RLIMIT_NOFILE => 30,
 	},
 	loggrep => {
-	    qr/syslogd: receive_fd: recvmsg: Message too long/ => 4,
+	    qr/syslogd: receive_fd: recvmsg: Message too long/ => 5,
 	    # One file is opened by test default config, 20 by multifile.
-	    qr/X FILE:/ => 1+16,
-	    qr/X UNUSED:/ => 4,
+	    qr/X FILE:/ => 1+15,
+	    qr/X UNUSED:/ => 5,
+	    qr/Accepting tcp connection/ => 0,
+	    qr/Listen again/ => '>=1',
 	},
     },
+    server => {
+	loggrep => { get_testlog() => 0 },
+    },
     multifile => [
-	(map { { loggrep => get_testlog() } } 0..15),
-	(map { { loggrep => { qr/./s => 0 } } } 16..19),
+	(map { { loggrep => qr/syslogd: accept deferred/ } } 0..14),
+	(map { { loggrep => { qr/./s => 0 } } } 15..19),
     ],
+    file => {
+	loggrep => qr/syslogd: accept deferred: Too many open files/,
+    },
+    pipe => { loggrep => {} },
 );
 
 1;
