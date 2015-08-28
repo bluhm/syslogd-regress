a0a33e51a27af960d656a3c68c373fdd97af9c6e
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-08-28 17:46:27 +0200

    Test various combinations of IPv4 and IPv6 hostname matching.

--- args-block-host-tcp6.pl
+++ args-block-host-tcp6.pl
@@ -1,5 +1,5 @@
-# The client writes a message to Sys::Syslog native method.
-# The syslogd writes into multiple files depending on hostname.
+# The client writes a message in TCP stream to ::1.
+# The syslogd writes into multiple files depending on IPv6-Adress.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via UDP to the loghost.
 # The server receives the message on its UDP socket.
@@ -12,24 +12,29 @@ use Socket;
 
 our %args = (
     client => {
-	connect => { domain => AF_INET, addr => "127.0.0.1", port => 514 },
+	connect => { domain => AF_INET6, proto => "tcp", addr => "::1",
+	    port => 514 },
     },
     syslogd => {
-	options => [ '-nu' ],
+	options => ["-n", "-T", "[::1]:514"],
 	conf => <<'EOF',
-+nonexist
++localhost
 *.*	$objdir/file-0.log
 +127.0.0.1
 *.*	$objdir/file-1.log
++::1
 *.*	$objdir/file-2.log
-+*
++$host
 *.*	$objdir/file-3.log
++*
+*.*	$objdir/file-4.log
 EOF
     },
     multifile => [
 	{ loggrep => { get_testgrep() => 0 } },
+	{ loggrep => { get_testgrep() => 0 } },
 	{ loggrep => { get_testgrep() => 1 } },
-	{ loggrep => { get_testgrep() => 1 } },
+	{ loggrep => { get_testgrep() => 0 } },
 	{ loggrep => { get_testgrep() => 1 } },
     ],
 );
