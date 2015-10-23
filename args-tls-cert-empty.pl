3feb588be68f9b14e19d0aa1bdbdd9d700c5a549
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-10-23 21:51:51 +0200

    Syslogd gets an empty TLS server certificate.

--- args-tls-cert-empty.pl
+++ args-tls-cert-empty.pl
@@ -1,47 +1,51 @@
-# The syslogd listens on 127.0.0.1 TLS socket.
-# The client writes a message into a 127.0.0.1 TLS socket.
-# The syslogd writes it into a file and through a pipe.
-# The syslogd passes it via UDP to the loghost.
-# The server receives the message on its UDP socket.
-# Find the message in client, file, pipe, syslogd, server log.
-# Check that the file log contains the hostname and message.
+# Syslogd gets an empty TLS server certificate.
+# The client cannot connect to 127.0.0.1 TLS socket.
+# Check that syslog log contains an error message.
 
 use strict;
 use warnings;
 use Socket;
 
+my $cert = "/etc/ssl/127.0.0.1:6514.crt";
+my @sudo = $ENV{SUDO} ? $ENV{SUDO} : ();
+my @cmd = (@sudo, "cp", "--", "empty", $cert);
+system(@cmd) and die "Command '@cmd' failed: $?";
+END {
+    my @cmd = (@sudo, "rm", "-f", "--", $cert);
+    system(@cmd) and warn "Command '@cmd' failed: $?";
+}
+
 our %args = (
     client => {
-	connect => { domain => AF_INET, proto => "tls", addr => "127.0.0.1",
-	    port => 6514 },
-	loggrep => {
-	    qr/connect sock: 127.0.0.1 \d+/ => 1,
-	    get_testgrep() => 1,
+	func => sub {
+	    my $self = shift;
+	    IO::Socket::INET6->new(
+		Domain              => AF_INET,
+		Proto               => "tcp",
+		PeerAddr            => "127.0.0.1",
+		PeerPort            => 6514,
+	    ) and die "tcp socket connect to 127.0.0.1:6514 succeeded";
 	},
+	nocheck => 1,
     },
     syslogd => {
 	options => ["-S", "127.0.0.1:6514"],
-	fstat => {
-	    qr/^root .* internet/ => 0,
-	    qr/^_syslogd .* internet/ => 3,
-	    qr/ internet stream tcp \w+ 127.0.0.1:6514$/ => 1,
-	},
 	ktrace => {
-	    qr{NAMI  "/etc/ssl/private/127.0.0.1:6514.key"} => 1,
-	    qr{NAMI  "/etc/ssl/private/127.0.0.1.key"} => 1,
 	    qr{NAMI  "/etc/ssl/127.0.0.1:6514.crt"} => 1,
-	    qr{NAMI  "/etc/ssl/127.0.0.1.crt"} => 1,
+	    qr{NAMI  "/etc/ssl/127.0.0.1.crt"} => 0,
 	},
 	loggrep => {
 	    qr{Keyfile /etc/ssl/private/127.0.0.1.key} => 1,
-	    qr{Certfile /etc/ssl/127.0.0.1.crt} => 1,
+	    qr{Certfile $cert} => 1,
+	    qr{syslogd: tls_configure server} => 2,
 	},
     },
-    file => {
-	loggrep => {
-	    qr/ localhost /. get_testgrep() => 1,
-	},
+    server => {
+	noserver => 1,
     },
+    file => { nocheck => 1 },
+    pipe => { nocheck => 1 },
+    tty => { nocheck => 1 },
 );
 
 1;
