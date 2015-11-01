d92ff24f06d9117624a34523f0aa08b6f87d0a7a
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-11-02 00:16:13 +0100

    Replace TCP with TLS.

--- args-rsyslog-client-tls.pl
+++ args-rsyslog-client-tls.pl
@@ -1,6 +1,6 @@
-# Test TCP with rsyslogd as sender.
+# Test TLS with rsyslogd as sender.
 # The client writes a message to rsyslogd UDP socket.
-# The rsyslogd forwards the message to syslogd TCP listen socket.
+# The rsyslogd forwards the message to syslogd TLS listen socket.
 # The syslogd writes it into a file and through a pipe.
 # The syslogd passes it via UDP to the rsyslogd.
 # The rsyslogd receives the message on its UDP socket.
@@ -17,11 +17,11 @@ our %args = (
     },
     rsyslogd => {
 	listen => { domain => AF_INET, proto => "udp", addr => "127.0.0.1" },
-	connect => { domain => AF_INET, proto => "tcp", addr => "127.0.0.1",
-	    port => 514 },
+	connect => { domain => AF_INET, proto => "tls", addr => "127.0.0.1",
+	    port => 6514 },
     },
     syslogd => {
-	options => ["-T", "127.0.0.1:514"],
+	options => ["-S", "127.0.0.1"],
     },
 );
 
