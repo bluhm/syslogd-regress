0e6e8537f5b9da1c51cfbc3c7ac6984db8869faa
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-03-22 00:08:39 +0100

    Check that flags end in the sendsyslog() system call.

--- args-sendsyslog-flags.pl
+++ args-sendsyslog-flags.pl
@@ -4,16 +4,17 @@
 # The server receives the message on its UDP socket.
 # Find the message in client, file, pipe, syslogd, server log.
 # Create a ktrace dump of the client and check that sendsyslog(2)
-# has been used.
+# has been used with flags.
 
 use strict;
 use warnings;
+use Sys::Syslog 'LOG_CONS';
 
 our %args = (
     client => {
-	connect => { domain => "sendsyslog", flags => 0 },
+	connect => { domain => "sendsyslog", flags => LOG_CONS },
 	ktrace => {
-	    qr/CALL  sendsyslog\(/ => 2,
+	    qr/CALL  sendsyslog\(.*,0x2\)/ => 1,
 	    qr/GIO   fd -1 wrote \d+ bytes/ => 2,
 	    qr/RET   sendsyslog 0/ => 2,
 	},
