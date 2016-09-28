73caf0b394fbc34de73cc0f0028e10d1cfcb0f5b
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-09-28 23:05:02 +0200

    Check the time format in server and file.

--- args-zulu.pl
+++ args-zulu.pl
@@ -1,4 +1,3 @@
-# Test with default values, that is:
 # The client writes a message to Sys::Syslog native method.
 # The syslogd writes it into a file and through a pipe and to tty.
 # The syslogd passes it via UDP to the loghost.
@@ -7,8 +6,39 @@
 
 use strict;
 use warnings;
+use Sys::Hostname;
+
+(my $host = hostname()) =~ s/\..*//;
+
+# 2016-09-28T15:38:09Z
+my $iso = qr/20\d\d-\d\d-\d\dT\d\d:\d\d:\d\dZ/;
+my $bsd = qr/\w\w\w [ \d]\d \d\d:\d\d:\d\d/;
 
 our %args = (
+    client => {
+	connect => { domain => AF_UNIX },
+	func => sub {
+	    my $self = shift;
+	    write_message($self, "no time");
+	    write_message($self, "Sep 28 17:37:51 bsd time");
+	    write_log($self);
+	},
+    },
+    syslogd => {
+	options => ["-z"],
+    },
+    server => {
+	loggrep => {
+	    qr/>$iso no time$/ => 1,
+	    qr/>$iso bsd time$/ => 1,
+	},
+    },
+    file => {
+	loggrep => {
+	    qr/^$iso $host no time$/ => 1,
+	    qr/^$iso $host bsd time$/ => 1,
+	},
+    },
 );
 
 1;
