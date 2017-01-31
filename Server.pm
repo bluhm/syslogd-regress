a7edec99071597e08941d2283e53900b9cab831f
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-01-31 19:03:03 +0100

    cvs commit

--- Server.pm
+++ Server.pm
@@ -1,4 +1,4 @@
-#	$OpenBSD: Server.pm,v 1.10 2016/09/23 14:35:15 bluhm Exp $
+#	$OpenBSD: Server.pm,v 1.11 2017/01/31 18:02:54 bluhm Exp $
 
 # Copyright (c) 2010-2015 Alexander Bluhm <bluhm@openbsd.org>
 #
