2a66e671b7f1a6b31417ac2a3ab9a72caa4c45d2
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-02-09 23:13:26 +0100

    Test that only created sockets are removed.

---
+++ args-remove-unix.pl
@@ -0,0 +1,54 @@
+# The client writes messages to MAXUNIX unix domain sockets.
+# The syslogd -a writes them into a file and through a pipe.
+# The syslogd -a passes them via UDP to the loghost.
+# The server receives the messages on its UDP socket.
+# Find the message in client, file, pipe, syslogd, server log.
+# Check that the file log contains a message from every socket.
+# Check that no error is printed.
+
+use strict;
+use warnings;
+use Errno ':POSIX';
+use IO::Socket::UNIX;
+
+my @errors = (ENOTSOCK);
+my $errors = "(". join("|", map { $! = $_ } @errors). ")";
+
+{
+    unlink("file.sock");
+    open(my $fh, '>', "file.sock")
+	or die "Create 'file.sock' failed: $!";
+}
+
+our %args = (
+    client => {
+	func => sub {
+	    my $self = shift;
+	    write_unix($self, "unix.sock");
+	    eval { write_unix($self, "file.sock") };
+	    warn $@;
+	    write_shutdown($self);
+	},
+	loggrep => {
+	    qr/connect to file.sock unix socket failed: $errors/ => 1,
+	},
+    },
+    syslogd => {
+	options => [ "-a" => "unix.sock", "-a" => "file.sock" ],
+	loggrep => {
+	    qr/connect unix "file.sock": Socket is already connected/ => 1,
+	},
+    },
+    file => {
+	loggrep => {
+	    "unix.sock unix socket" => 1,
+	    "file.sock unix socket" => 0,
+	},
+    },
+    check => sub {
+	-e "unix.sock" and die "Unix socket 'unix.sock' exists";
+	-f "file.sock" or die "Plain file 'file.sock' missing";
+    },
+);
+
+1;
