8ebb4f2917d2999571fc9a4b33744dedbb4f0c11
Alexander Bluhm <alexander.bluhm@gmx.net>
2020-11-06 00:03:14 +0100

    Avoid Perl warning.

--- Client.pm
+++ Client.pm
@@ -47,7 +47,8 @@ sub child {
 
 	# TLS 1.3 writes multiple messages without acknowledgement.
 	# If the other side closes early, we want broken pipe error.
-	$SIG{PIPE} = 'IGNORE' if $self->{connectproto} eq "tls";
+	$SIG{PIPE} = 'IGNORE' if defined($self->{connectdomain}) &&
+	    $self->{connectproto} eq "tls";
 
 	if (defined($self->{connectdomain}) &&
 	    $self->{connectdomain} ne "sendsyslog") {
